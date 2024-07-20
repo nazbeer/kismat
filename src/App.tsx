90e1a5a5844136c284a6a0d824b7fce236454630
Nasbeer Ahammed <nasbeer@mitesolutions.com>
2024-07-20 18:29:52 +0530

    kismat landing page react

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import LandingPage from './pages/LandingPage'
+import './index.css'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <LandingPage />
+    </div>
+  )
+}
+
+export default App
